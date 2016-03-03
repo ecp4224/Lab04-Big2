0a913ac03c5d20a7ca96e4e557aa6acdf0f34462
Andrew Corp <corpa@wit.edu>
2016-03-03 10:30:41 -0500

    Made Classes, and finished Card

--- src/Card.java
+++ src/Card.java
@@ -6,8 +6,43 @@ public class Card {
     private int value;
     private int suit;
 
-    public Card() {
+    public final int DIAMONDS = 0;
+    public final int CLUBS = 1;
+    public final int HEARTS = 2;
+    public final int SPADES = 3;
 
+    public Card() {
+        suit = SPADES;
+        value = 1;
+    }
+
+    public String getSuitAsString(){
+        switch (suit) {
+            case SPADES: return "Spades";
+            case HEARTS: return "Hearts";
+            case CLUBS: return "Clubs";
+            case DIAMONDS: return "Diamonds";
+            default: return "null";
+        }
+    }
+
+    public String getValueAsString() {
+        switch (value){
+            case 1: return "3";
+            case 2: return "4";
+            case 3: return "5";
+            case 4: return "6";
+            case 5: return "7";
+            case 6: return "8";
+            case 7: return "9";
+            case 8: return "10";
+            case 9: return "Jack";
+            case 10: return "Queen";
+            case 11: return "King";
+            case 12: return "Ace";
+            case 13: return "2";
+            default: return null;
+        }
     }
 
     public int getValue() {
---
+++ src/Deck.java
@@ -0,0 +1,11 @@
+/**
+ * Created by corpa on 3/3/2016.
+ */
+public class Deck {
+
+
+
+    public Deck() {
+
+    }
+}
