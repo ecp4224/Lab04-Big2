cd3c8c3b77e474957e7c34d1d5add8c5060345f2
Andrew Corp <corpa@wit.edu>
2016-03-17 17:51:22 -0400

    Should be Good

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class HumanPlayer extends Player {
     @Override
     public List<Card> makeMove(Game game) {
-        //TODO Scanner stuff Returns player
         List<Card> middleCards = game.getCurrentMiddle();
         List<Card> cardsToPlay = new List<>();
+
         Scanner in = new Scanner(System.in);
+        for(Card c : middleCards){
+            System.out.println(c.toString());
+        }
         printHand();
 
         System.out.println("Type pass if you would like to pass: ");
@@ -18,16 +21,23 @@ public class HumanPlayer extends Player {
             return null;
         } else {
             String[] arrayCardInput = cardInput.replace(" ", "").split(",");//"2C","6D"
-            for(String s : arrayCardInput){
-
-                if(Integer.parseInt(s.charAt(0)+"") > 15 || Integer.parseInt(s.charAt(0) + "") < 3){
-                    System.out.println("Re enter your cards!");
+            if(arrayCardInput.length >= 5) {
+                System.out.println("Invalid amount of cards played, Re-try Move.");
+                return makeMove(game);
+            }
+            for(String s : arrayCardInput) {
+                if (Integer.parseInt(s.charAt(0) + "") > 15 || Integer.parseInt(s.charAt(0) + "") < 3) {
+                    System.out.println("Invalid Move Entered, Value is not correct! Re-try Move.");
                     return makeMove(game);
+                } else if (s.charAt(1) != 'D' || s.charAt(1) != 'S' || s.charAt(1) != 'C' || s.charAt(1) != 'H') {
+                    System.out.println("Invalid Move Entered, Suit is not correct! Re-try Move.");
+                    return makeMove(game);
+                } else {
+                    Card c = new Card(Integer.parseInt(s.charAt(0) + ""), s.charAt(1));
+                    cardsToPlay.add(c);
                 }
-                Card c = new Card(Integer.parseInt(s.charAt(0) + ""),s.charAt(1));
-                cardsToPlay.add(c);
             }
         }
-        return null;
+        return cardsToPlay;
     }
 }
